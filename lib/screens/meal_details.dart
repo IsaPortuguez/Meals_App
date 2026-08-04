2d1ed6cf8a29c5125d06f1a84e453595f857fc16
IsaPortuguez <isabelportuguez.dev@gmail.com>
2026-08-04 10:45:46 -0600

    Add Implicit Animation

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -37,7 +37,22 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(
+                    begin: 0.5,
+                    end: 1,
+                  ).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           ),
         ],
       ),
